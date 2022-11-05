84d638f346a90c5778d84ad7fa815c4c194d8605
dedmc95@gmail.com <dedmc95@gmail.com>
2022-11-04 17:51:27 -0700

    First commit containing my director program. Feel free to alter it if some variable are different or if I forgot to inlcude something.

--- greed/greed/game/directing/director.py
+++ greed/greed/game/directing/director.py
@@ -11,7 +11,7 @@ class Director:
     """
 
     def __init__(self, keyboard_service, video_service):
-        """Constructs a new Director using the specified keyboard service, video service and scoring.
+        """Constructs a new Director using the specified keyboard and video services.
         
         Args:
             keyboard_service (KeyboardService): An instance of KeyboardService.
@@ -19,13 +19,12 @@ class Director:
         """
         self._keyboard_service = keyboard_service
         self._video_service = video_service
-        self._scoring = Scoring() # This intantiates an instance of the 'Scoring()' class so we can access it's methods and variables
         
-    def start_game(self, cast): # This recieves the 'cast' instantiation from '__main__.py'. It's an instance of our 'Cast()' class that's been populated.
+    def start_game(self, cast):
         """Starts the game using the given cast. Runs the main game loop.
 
         Args:
-            cast (Cast): The cast of actors passed from __main__.py.
+            cast (Cast): The cast of actors.
         """
         self._video_service.open_window()
         while self._video_service.is_window_open():
@@ -35,36 +34,40 @@ class Director:
         self._video_service.close_window()
 
     def _get_inputs(self, cast):
-        """Gets directional input from the keyboard and applies it to the robot.
+        """Gets directional input from the keyboard and applies it to the player's icon.
         
         Args:
             cast (Cast): The cast of actors.
         """
-        robot = cast.get_first_actor("robots")
+        PlayerIcon = cast.get_first_actor("player icon")
         velocity = self._keyboard_service.get_direction()
-        robot.set_velocity(velocity)        
+        PlayerIcon.set_velocity(velocity)        
 
     def _do_updates(self, cast):
-        """Updates the robot's position and resolves any collisions with artifacts.
+        """Updates the player icon's position and resolves any collisions with artifacts.
         
         Args:
             cast (Cast): The cast of actors.
         """
         banner = cast.get_first_actor("banners")
-        robot = cast.get_first_actor("robots")
-        artifacts = cast.get_actors("artifacts") # Uses our instance of 'cast' passed from '__main__.py' and runs the 'get_actors()' function passing the 'artifact' key to get the artifacts.
+        PlayerIcon = cast.get_first_actor("player icon")
+        gems = cast.get_actors("gems")
+        rocks = cast.get.actors("rocks")
 
-        banner.set_text(f"Score: {self._scoring.get_score()}") # This is where we will set our "Score: " banner. I accessed the 'get_score()' method directly.
+        banner.set_text("")
         max_x = self._video_service.get_width()
         max_y = self._video_service.get_height()
-        robot.move_next(max_x, max_y)
+        PlayerIcon.move_next(max_x, max_y)
         
-        for artifact in artifacts:
-            if robot.get_position().equals(artifact.get_position()): # When the robot and artifact are in the same position, it will run the 'set_score()' function on the next line. We are calling 'get_position()' from our Actor() class.
-                self._scoring.set_score(cast) # Passes the 'cast' instance to scoring. After running this function, the score is changed and reflected in our 'banner.set_text()' line above.
+        for gems in gems:
+            if PlayerIcon.get_position().equals(gems.get_position()):
+                message = gems.get_message()
+                banner.set_text(message) 
 
-
-                #banner.set_text(f"Score: {self._scoring.get_score()}") # Don't need this line anymore because it updates the 'banner.set_text()' line earlier in the loop.   
+        for rocks in rocks:
+              if PlayerIcon.get_position().equals(rocks.get_position()):
+                message = rocks.get_message()
+                banner.set_text(message)  
         
     def _do_outputs(self, cast):
         """Draws the actors on the screen.
