28e99197e5adb5f33c85c471aaef89c63510b0c5
mvrush <mvrush@gmail.com>
2022-11-09 21:14:33 -0700

    roll back director, update scoring

--- greed/greed/game/casting/scoring.py
+++ greed/greed/game/casting/scoring.py
@@ -7,21 +7,14 @@ class Scoring:
         #self._cast = Cast() # Don't need this anymore This creates an instance (instantiates) of our Cast() class giving us access to it's methods and public variables (I don't think the private ones)
         self._total_score = 0 # This adds another Attribute found only in our Scoring() class called 'self._total_score'. It holds our cumulative score
     
-    def set_score(self, cast): # recieves the 'cast' instance from the call in 'director.py' which is actually an instance of our 'Cast()' class instantiaed in __main__.py
-        #self._total_score += 100 # This line for testing only
-
-        artifacts = cast.get_actors("artifacts") #  __main__.py runs first and uses an instance of the 'Cast()' class called 'class' That's where it's populated. This passes the 'artifacts' key to 'get_actors()' in the 'cast' insance of 'Cast()' class
-        # print(artifacts) # prints the list of the object values of 'artifacts' in the cast which it pulls from the 'director.py' from an instance of 'Cast()' found in __main__.py. Uses the 'get_text()' function from the 'Actor()' class.
-        for artifact in artifacts:
-            #print(artifact.get_text()) # I think I am calling the 'get_text()' function from the Actor() class to pull the text out of the artifact object.
-                # To fix this next part I think I need to create an array or something with all the * and 0 values. Somehow I need to check only a single artifact in one position and it seems
-                # to be looping through all of them continuously.
-            if artifact.get_text() == "*":
-                print("It was an astrisk")
-                self._total_score += 1
-            elif artifact.get_text() == "0":
-                print("It was a zero")
-                self._total_score -= 1
+    # New way of doing it where it just passes the 'text' value of the matched artifact
+    def set_score(self, text):
+        if text == "*":
+            print("It was an astrisk")
+            self._total_score += 1
+        elif text == "0":
+            print("It was a zero")
+            self._total_score -= 1
         
         return self._total_score
     
--- greed/greed/game/directing/director.py
+++ greed/greed/game/directing/director.py
@@ -11,7 +11,7 @@ class Director:
     """
 
     def __init__(self, keyboard_service, video_service):
-        """Constructs a new Director using the specified keyboard and video services.
+        """Constructs a new Director using the specified keyboard service, video service and scoring.
         
         Args:
             keyboard_service (KeyboardService): An instance of KeyboardService.
@@ -19,12 +19,13 @@ class Director:
         """
         self._keyboard_service = keyboard_service
         self._video_service = video_service
+        self._scoring = Scoring() # This intantiates an instance of the 'Scoring()' class so we can access it's methods and variables
         
-    def start_game(self, cast):
+    def start_game(self, cast): # This recieves the 'cast' instantiation from '__main__.py'. It's an instance of our 'Cast()' class that's been populated.
         """Starts the game using the given cast. Runs the main game loop.
 
         Args:
-            cast (Cast): The cast of actors.
+            cast (Cast): The cast of actors passed from __main__.py.
         """
         self._video_service.open_window()
         while self._video_service.is_window_open():
@@ -34,40 +35,37 @@ class Director:
         self._video_service.close_window()
 
     def _get_inputs(self, cast):
-        """Gets directional input from the keyboard and applies it to the player's icon.
+        """Gets directional input from the keyboard and applies it to the robot.
         
         Args:
             cast (Cast): The cast of actors.
         """
-        PlayerIcon = cast.get_first_actor("player icon")
+        robot = cast.get_first_actor("robots")
         velocity = self._keyboard_service.get_direction()
-        PlayerIcon.set_velocity(velocity)        
+        robot.set_velocity(velocity)        
 
     def _do_updates(self, cast):
-        """Updates the player icon's position and resolves any collisions with artifacts.
+        """Updates the robot's position and resolves any collisions with artifacts.
         
         Args:
             cast (Cast): The cast of actors.
         """
         banner = cast.get_first_actor("banners")
-        PlayerIcon = cast.get_first_actor("player icon")
-        gems = cast.get_actors("gems")
-        rocks = cast.get.actors("rocks")
+        robot = cast.get_first_actor("robots")
+        artifacts = cast.get_actors("artifacts") # Uses our instance of 'cast' passed from '__main__.py' and runs the 'get_actors()' function passing the 'artifact' key to get the artifacts.
 
-        banner.set_text("")
+        banner.set_text(f"Score: {self._scoring.get_score()}") # This is where we will set our "Score: " banner. I accessed the 'get_score()' method directly.
         max_x = self._video_service.get_width()
         max_y = self._video_service.get_height()
-        PlayerIcon.move_next(max_x, max_y)
+        robot.move_next(max_x, max_y)
         
-        for gems in gems:
-            if PlayerIcon.get_position().equals(gems.get_position()):
-                message = gems.get_message()
-                banner.set_text(message) 
+        for artifact in artifacts:
+            if robot.get_position().equals(artifact.get_position()): # When the robot and artifact are in the same position, it will run the 'set_score()' function on the next line. We are calling 'get_position()' from our Actor() class.
+                #self._scoring.set_score(cast) # (old way I was doing it) Passes the 'cast' instance to scoring. After running this function, the score is changed and reflected in our 'banner.set_text()' line above.
+                self._scoring.set_score(artifact.get_text()) # Passes the value of 'text' from the matched artifact to scoring. After running this function, the score is changed and reflected in our 'banner.set_text()' line above.
 
-        for rocks in rocks:
-              if PlayerIcon.get_position().equals(rocks.get_position()):
-                message = rocks.get_message()
-                banner.set_text(message)  
+
+                #banner.set_text(f"Score: {self._scoring.get_score()}") # Don't need this line anymore because it updates the 'banner.set_text()' line earlier in the loop.   
         
     def _do_outputs(self, cast):
         """Draws the actors on the screen.
