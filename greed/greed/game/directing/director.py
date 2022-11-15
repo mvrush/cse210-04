968a70ba73206ac1d13e5de7072737662995f5d6
mvrush <mvrush@gmail.com>
2022-11-14 22:03:53 -0700

    fully implement separate rock and gem classes

--- greed/greed/game/directing/director.py
+++ greed/greed/game/directing/director.py
@@ -1,15 +1,8 @@
-import random # import the random library to use below
-
 from game.casting.scoring import Scoring
 from game.shared.point import Point # Had to import this so I could use Point() class to set velocity for the artifacts.
-from game.shared.color import Color # Had to import to regenerate artifacts
-from game.casting.artifact import Artifact # Had to import this so I could manipulate the Rocks and Gems when removing and adding them after they are caught.
+from game.casting.gem import Gem # imports the Gem class so I can use it during regeneration and deletion
+from game.casting.rock import Rock # imports the Gem class so I can use it during regeneration and deletion
 
-### To regenerate artifacts I need these variable values
-CELL_SIZE = 15
-FONT_SIZE = 15
-COLS = 60 # This splits the width (x) into 60 columns
-ROWS = 40 # This splits the height (y) into 40 rows
 
 class Director:
     """A person who directs the game. 
@@ -31,6 +24,8 @@ class Director:
         self._keyboard_service = keyboard_service
         self._video_service = video_service
         self._scoring = Scoring() # This intantiates an instance of the 'Scoring()' class so we can access it's methods and variables
+        self._gem = Gem()
+        self._rock = Rock()
         
     def start_game(self, cast): # This recieves the 'cast' instantiation from '__main__.py'. It's an instance of our 'Cast()' class that's been populated.
         """Starts the game using the given cast. Runs the main game loop.
@@ -85,32 +80,11 @@ class Director:
                 self._scoring.set_score(artifact.get_text()) # Passes the value of 'text' from the matched artifact to scoring. After running this function, the score is changed and reflected in our 'banner.set_text()' line above.
                 cast.remove_actor("artifacts", artifact) # This removes the artiface when the robot and artifact intersect.
                 
-                ### Add new artifact when the robot position and artifact position are the same (replaces the removed ones)
-                # I just took lines 103-125 right out of the CREATE ARTIFACT section of the __main__.py file
-                text = random.choice(['*', '0']) # This line uses the random.choice() function to choose between the string '*' or '0' {zero}.
-                #message = messages[n] # We won't be using messages anymore
-
-                x = random.randint(1, COLS - 1) # This sets a random value for our x (horizontal) position in the columns
-                y = random.randint(1, ROWS - 1) # This sets a random value for our y (vertical) position in the rows
-                position = Point(x, y) # this will be the position for one artifact as it loopws.
-                position = position.scale(CELL_SIZE) # sets the position size to equal what we defined for our CELL_SIZE
-
-                # The next 4 lines create a random color for each artifact
-                r = random.randint(0, 255)
-                g = random.randint(0, 255)
-                b = random.randint(0, 255)
-                color = Color(r, g, b)
-                
-                # Calls our RockGem() class and sets each of the values below using the methods in the Artifact() class
-                artifact = Artifact()
-                artifact.set_text(text)
-                artifact.set_font_size(FONT_SIZE)
-                artifact.set_color(color)
-                artifact.set_position(position)
-                #artifact.set_message(message) # We don't need a message anymore.
-                cast.add_actor("artifacts", artifact)
-                
-        ###
+                ### the following is a loop within the loop to replace the removed rock or gem.
+                if artifact.get_text() == "*": # looks at the value of the text for each artifact in loop
+                    self._gem.add_remove_artifact(cast) # this will add an '*'
+                else:
+                    self._rock.add_remove_artifact(cast) # this will add an '0'
         
     def _do_outputs(self, cast):
         """Draws the actors on the screen.
